053cc70e045bae1ed2bd5451497c5312e8fa28c3
Oluwatobiloba Oduntan <Oduntant@gmail.com>
2021-11-14 12:13:00 -0500

    Adding files

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,8 +1,55 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+// // Create the map object with a center and zoom level.
+// let map = L.map('mapid').setView([36.1733, -120.1794], 5);
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+      console.log(layer);
+      layer.bindPopup();
+    }
+
+  }).addTo(map);
+
+// // Coordinates for each point to be used in the line.
+// let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790],
+//     [40.7899, -111.9791],
+//     [47.4502, -122.3088]
+//   ];
+
+// // Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+//     color: "yellow"
+//   }).addTo(map);
 
 
 // // Add a marker to the map for Los Angeles, California.
@@ -14,9 +61,7 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 // Get data from cities.js
 let cityData = cities;
-    radius: 300
-    color: "orange"
-    fillColor: '#ffffa1'
+    
 
 
 // // An array containing each city's location, state, and population.
@@ -80,8 +125,13 @@ cityData.forEach(function(city) {
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    accessToken: API_KEY
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY,
 });
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+
